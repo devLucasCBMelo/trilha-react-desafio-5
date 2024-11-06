2358d5b43951501b58f9d0bff8755eac25fc3197
Lucas Costa <englucascbmelo@gmail.com>
2024-11-06 11:16:57 -0300

    trocando URL da API

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://ubnjujurwqiubeofogpl.supabase.co/rest/v1/',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibmp1anVyd3FpdWJlb2ZvZ3BsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4ODkxODMsImV4cCI6MjA0NjQ2NTE4M30.fI4jNrrce7mW_7mH-ZM6YB2toYtQCAnM3AO2VyxPVwo",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibmp1anVyd3FpdWJlb2ZvZ3BsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4ODkxODMsImV4cCI6MjA0NjQ2NTE4M30.fI4jNrrce7mW_7mH-ZM6YB2toYtQCAnM3AO2VyxPVwo"
     }
 })
